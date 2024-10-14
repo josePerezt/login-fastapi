f4c9e485dc1e0b2ec60c519139193db1b1f829a8
joseDev <josemptj@gmail.com>
2024-10-14 13:33:20 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI
+
+app = FastAPI(
+  title="Login",
+  docs_url="/",
+  description="This is a app of login"
+)
+
+@app.get("/users")
+async def get_all_users():
+  return {"message":"Todos los usuarios"}
+
+
+@app.get("/users/{user_id}")
+async def get_user_by_id(user_id:int):
+  return {"message":"usuario por id"}
+
+
+@app.post("/users")
+async def create_user():
+  return {"message":"usuario creado"}
+
+
+@app.put("/users/{user_id}")
+async def create_user(usr_id:int):
+  return {"message":"Usuario actualizado"}
+
+
+@app.delete("/users/{user_id}")
+async def get_all_users(user_id:int):
+  return {"message":"usuario eliminado"}
